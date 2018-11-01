51f577e0c583276c37f55d528d331f1301bcf631
holysabre <1026579245@qq.com>
2018-11-01 17:35:04 +0800

    Update basci user profile editing

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
 
 class UserRequest extends FormRequest
 {
@@ -24,7 +26,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name',
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
         ];
