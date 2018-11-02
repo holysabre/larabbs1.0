e02e0540966864ca514a096fd9a2464aad77b77c
holysabre <1026579245@qq.com>
2018-11-02 15:16:01 +0800

    User can upload avatar

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -22,7 +22,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" type="button" id="dropdownMenu" data-toggle="dropdown">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                                <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
