b9e192d334aefe0aede34db543f3a1dd1c0b10f0
holysabre <1026579245@qq.com>
2018-11-05 16:39:43 +0800

    Seeding topics and users data

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence;
+    $text = $faker->text;
+    //随机取一个月内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    //随机取一个月内的时间，并比修改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $text,
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
