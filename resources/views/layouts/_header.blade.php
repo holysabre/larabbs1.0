6ce4cce5b2cace9de04938e53d296251d6e4b184
holysabre <1026579245@qq.com>
2018-11-06 14:38:15 +0800

    List topics created by user

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -32,11 +32,23 @@
                         </a>
                         <ul class="dropdown-menu">
                             <li>
+                                <a href="{{ route('users.show', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                    个人中心
+                                </a>
+                            </li>
+                            <li>
                                 <a href="{{ route('users.edit', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                                     编辑资料
                                 </a>
                             </li>
-                            <li><a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a></li>
+                            <li>
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                    <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
+                                    退出登录
+                                </a>
+                            </li>
                             <form action="{{ route('logout') }}" method="post" id="logout-form" style="display: none;">
                                 {{ csrf_field() }}
                             </form>
